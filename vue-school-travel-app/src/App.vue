bfe756a125650728668b416aa3a9e0797cab130c
Ayaz Baloch <ayazbaloch@hotmail.com>
2023-04-24 12:24:16 -0700

    committing the changes to the repo

--- vue-school-travel-app/src/App.vue
+++ vue-school-travel-app/src/App.vue
@@ -3,20 +3,16 @@
 </script>
 
 <template>
-  <img src="/vite.svg"  alt="Vite logo" />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  
+  <div class="container">
+    <router-view></router-view>
+  </div>
+  
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vue-school-travel-app/vite.config.js
+++ vue-school-travel-app/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path=require('path')
+import { resolve } from 'path';
+//const path=require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve:{
     alias : {
-      '@' : path.resolve(__dirname,'./src')
-    }
+      '@' : resolve(__dirname,'./src')
+    },
   },
   plugins: [vue()],
 })
